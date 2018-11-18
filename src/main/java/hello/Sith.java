7dd02f570a800bfcdcb52092351f00f14a0c37ea
Terrance <terrance.hanlon@ndsu.edu>
2018-11-18 01:40:23 -0600

    add spring dependencies

--- src/main/java/hello/Sith.java
+++ src/main/java/hello/Sith.java
@@ -1,5 +1,13 @@
 package hello;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sith")
 public class Sith {
 
 }
--- src/main/java/hello/SithController.java
+++ src/main/java/hello/SithController.java
@@ -1,5 +1,15 @@
 package hello;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping(path="/sith")
 public class SithController {
 
 }
